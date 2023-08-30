dffa33c80109bcc61c6786536b12e0686579d616
niklas-hjelm <niklas.hjelm@iths.se>
2023-08-30 14:17:09 +0200

    Add project files.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Skriv ett program som frågar efter användarens namn och skriver ut en hälsning på konsolen.
+//Om namnet är "David" ska det skriva ut "Hej David!"
+
+//Fråga användaren efter ett namn
+Console.WriteLine("Vad heter du?");
+
+//Ta emot ett namn
+string name = "";
+name = Console.ReadLine();
+
+//Om det inskrivna namnet är "David"
+if (name == "David" || name == "Niklas")
+{
+    // Skriv ut: "Hej David!"
+    Console.WriteLine("Hej David!");
+}
---
+++ LiveVilkorOchLoopar2023/Program.cs
@@ -0,0 +1,88 @@
+﻿//int age = 36;
+//string name = "Niklas";
+
+//if (name == "Niklas")
+//{
+//    Console.WriteLine("Niklas du är snäll!");
+//}
+//else if (age > 35)
+//{
+//        Console.WriteLine("oof!");
+//}
+//else
+//{
+//    Console.WriteLine("Vem är du?");
+//}
+
+//Console.WriteLine("_____________________");
+
+//int counter = 0;
+
+//while (name == "Niklas")
+//{
+//    Console.WriteLine(counter);
+//    if (counter > 10)
+//    {
+//        break;
+//    }
+//    Console.WriteLine("LOL");
+//    counter = counter + 1;
+//}
+//Console.WriteLine("_____________________");
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine(i);
+//    if (name != "Niklas")
+//    {
+//        break;
+//    }
+//    Console.WriteLine("LOL");
+//}
+
+int[] goodNumbers;
+goodNumbers = new int[10];
+
+for (int i = goodNumbers.Length - 1; i >= 0; i--)
+{
+    goodNumbers[i] = 10 - i;
+}
+
+foreach (int number in goodNumbers)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("_____________________");
+
+Array.Sort(goodNumbers);
+
+foreach (int number in goodNumbers)
+{
+    Console.WriteLine(number);
+}
+
+//foreach (int number in goodNumbers)
+//{
+//    Console.WriteLine(number);
+//}
+
+//int index = 0;
+
+//while (index < goodNumbers.Length)
+//{
+//    index++;
+//}
+
+//string name = "Niklas";
+
+//name = name + "!";
+
+//Console.WriteLine(name.Length);
+
+//Console.WriteLine(name[2]);
+
+//for (int i = 0; i < name.Length; i++)
+//{
+//    Console.WriteLine(name[i]);
+//}
+
